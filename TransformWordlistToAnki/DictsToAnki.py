e861775b567ff1e25f3734941d68f20e96c67c31
XiaoBAI <1052119089@qq.com>
2019-11-06 17:11:26 +0800

    Update DictsToAnki.py

--- TransformWordlistToAnki/DictsToAnki.py
+++ TransformWordlistToAnki/DictsToAnki.py
@@ -1,18 +1,15 @@
 import os
 lastchar = '+'
 tabs = 0
-file1 = open('a.txt', 'r')
+file1 = open('ass.txt', 'r', encoding = 'utf-8')
 with open('b.txt', 'w', encoding = 'utf-8') as file2:
     for line in file1 :
         if line not in ['\n', '\r\n'] and line[0] != '$' and line[0] != '@':
-            if line[0] != lastchar[0] :
-                if tabs == 2 :
-                    file2.write('\n')
-                    tabs = 0
-                else :
-                    file2.write('\t')
-                    tabs = tabs + 1
-            elif line[0] != '+' :
-                file2.write(';')
+            if line[0] == '+' :
+                file2.write('\n')
+            elif line[0] != lastchar[0] :
+                file2.write('\t')
+            
+            
             file2.write(line[1:len(line)-1])
             lastchar = line[0]
